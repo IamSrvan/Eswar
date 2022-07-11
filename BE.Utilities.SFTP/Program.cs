f46d13595d07118519b65c3bf3b285717caf7b9a
Saravanan <109074777+IamSrvan@users.noreply.github.com>
2022-07-11 22:25:31 +0530

    Add files via upload
    Uploaded in https://github.com/IamSrvan/Eswar/upload/main

---
+++ BE.Utilities.SFTP/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using WinSCP;
+using System.Configuration;
+ 
+
+namespace BE.Utilities.SFTP
+{
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Setup session options
+                string hostName = ConfigurationManager.AppSettings["Domain"].ToString();
+                string userName = ConfigurationManager.AppSettings["User"].ToString();
+                string password = ConfigurationManager.AppSettings["Password"].ToString();
+                string fingerprint = ConfigurationManager.AppSettings["Fingerprint"].ToString();
+                
+                SessionOptions sessionOptions = new SessionOptions
+                {
+                    Protocol = Protocol.Sftp,
+                    HostName = hostName,
+                    UserName = userName,
+                    Password = password,
+                    SshHostKeyFingerprint = fingerprint
+                };
+
+                
+                using (Session session = new Session())
+                {
+                    FTPLog.logger.Info("Starting SFTP transmisson...");
+                    FTPLog.logger.Info("Opening SFTP Session...");
+
+                    // Connect
+                    session.Open(sessionOptions);
+
+                    FTPLog.logger.Info("Uploading file(s)...");
+                    
+                    // Upload files
+                    TransferOptions transferOptions = new TransferOptions();
+                    transferOptions.TransferMode = TransferMode.Binary;
+                    transferOptions.FilePermissions = null; // This is default
+                    transferOptions.PreserveTimestamp = false;
+
+                    TransferOperationResult transferResult;
+                    string UploadFromLocation = ConfigurationManager.AppSettings["LocationUploadFrom"].ToString();
+                    string UploadToLocation = ConfigurationManager.AppSettings["LocationUploadTo"].ToString();
+                    transferResult = session.PutFiles(@UploadFromLocation, UploadToLocation, false, transferOptions);
+
+                    // Throw on any error
+                    transferResult.Check();
+
+                    // Log results
+                    foreach (TransferEventArgs transfer in transferResult.Transfers)
+                    {
+                        FTPLog.logger.Info(String.Format("Upload of {0} succeeded", transfer.FileName));
+                    }
+                }
+                FTPLog.logger.Info("Finished uploading file(s)");
+            }
+            catch (Exception e)
+            {
+                FTPLog.logger.Error(String.Format("Error: {0}", e.ToString()));
+            }
+            WaitConsole();
+        }
+
+        private static void WaitConsole()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("...done...");
+            Console.ReadKey();
+        }
+    }
+
+
+    public class FTPLog
+    {
+        public static readonly ILog logger = 
+              LogManager.GetLogger(typeof(FTPLog));
+    
+        static FTPLog()
+        {
+            XmlConfigurator.Configure();
+        }
+    }
+}
---
+++ BE.Utilities.SFTP/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BE.Utilities.SFTP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BE.Utilities.SFTP")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("01015f22-11de-4e8c-96cc-8f26e09750e9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+// This will cause log4net to look for configuration file
+// called [ThisApp].exe.config in the application base
+// directory (i.e the directory containing [ThisApp].exe)
+// The config file will be watched for changes
+[assembly: log4net.Config.XmlConfigurator(Watch=true)]
